b4a429defb12a25f391938b3309ffdb77fd7fcd7
Tom Maher <tmaher@heroku.com>
2012-11-15 18:22:29 -0800

    wip

--- lib/woof_util.rb
+++ lib/woof_util.rb
@@ -1,19 +1,28 @@
 class WoofUtil
 
-  def self.shellout(*args)
+  # usage
+  # shellout "xargs", :stdin => "/etc", :args => ["ls", "-ld"]
+  def self.shellout(cmd, params = {})
+    params[:stdin] ||= ""
+    params[:args] ||= []
+    
     parent_r,child_w=IO.pipe ; parent_r.binmode ; child_w.binmode
-    #child_r,parent_w=IO.pipe ; child_r.binmode ; parent_w.binmode
+    child_r,parent_w=IO.pipe ; child_r.binmode ; parent_w.binmode
 
     pid = fork do
       parent_r.close
-      $stdin.reopen "/dev/null"
+      parent_w.close
+      $stdin.reopen child_r
       $stdout.reopen child_w
       $stderr.reopen child_w
       # should be closing other open FDs, punting on it for now
-      exec(*args)
+      exec *([cmd].concat params[:args])
     end
     child_w.close
+    child_r.close
     buf = []
+    parent_w.write params[:stdin]
+    parent_w.close
     while Process.waitpid(pid, Process::WNOHANG).nil? do
       buf.push(parent_r.read 4096)
     end
